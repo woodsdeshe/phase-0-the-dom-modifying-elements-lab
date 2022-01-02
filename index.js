e0e7f7d1d437f75608c77d1b191494791fe17c43
DeShe Woods <woodsdeshe94@gmail.com>
2022-01-02 17:06:31 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory";
+newHeader.innerHTML = '\"YOUR-NAME is the champion\"';
+
